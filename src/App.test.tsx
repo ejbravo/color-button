876aabb3442f77e9caa1b8246fe62964752dc033
AMBES000090 <ambes000090@MacBook-Pro-de-AMBES000090.local>
2021-12-15 13:09:20 +0100

    Color button initial tests: Success
     Committer: Eduardo Bravo  <10002456@ergo.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of "change to blue"
+  const colorButton = screen.getByRole('button', {name: /change to blue/i});
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
 });
+
+test('Button turns blue when clicked', () => {
+  render(<App />);
+
+  // expect the background color to red
+  const colorButton = screen.getByRole('button', {name: /change to blue/i});
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+  
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
+
+
+})
